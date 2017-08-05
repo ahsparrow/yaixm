e106fe8623b1bc7c464a39025d7854cb7d001b87
Alan Sparrow <github@freeflight.org.uk>
2017-08-05 20:47:21 +0100

    Update LoAs

--- yaixm/tests/yaixm_test.py
+++ yaixm/tests/yaixm_test.py
@@ -4,112 +4,136 @@ import tempfile
 import yaixm
 
 TEST_AIRSPACE = {
-  'header': {
-    'airac_date': "2017-05-12",
-    'author': "Bloggs",
-    'email': "foo@bar.com",
-    'release_timestamp': "2017-05-11T07:55:53+00:00",
-    'schema_version': 1
-  },
-  'airspace': [
-    {
-      'name': "BENSON",
-      'shape': [
+    'release': {
+        'airac_date': "2017-05-12",
+        'release_timestamp': "2017-05-11T07:55:53+00:00",
+        'schema_version': 1
+    },
+    'airspace': [
         {
-          'boundary': [
-            {
-              'circle': {
-                'centre': "513654N 0010545W",
-                'radius': "2 nm"
-              }
-            }
-          ],
-          'lower': "GND",
-          'upper': "2203 ft"
+            'name': "BENSON",
+            'geometry': [
+                {
+                    'boundary': [
+                        {
+                            'circle': {
+                                'centre': "513654N 0010545W",
+                                'radius': "2 nm"
+                            }
+                        }
+                    ],
+                    'lower': "GND",
+                    'upper': "2203 ft"
+                }
+            ],
+            "type": "ATZ"
         }
-      ],
-      "type": "ATZ"
-    }
-  ]
+    ],
+    "loa": [
+        {
+            "name": "LOA FOO",
+            "airspace": [{
+                "feature": {
+                    "name": "TEST BOX",
+                    "type": "CTR",
+                    "geometry": [{
+                        "lower": "GND",
+                        "upper": "1000 ft",
+                        "boundary" : [{
+                            'circle': {
+                                'centre': "513654N 0010545W",
+                                'radius': "2 nm"
+                            }
+                        }]
+                    }]
+                },
+                "mod_volumes": [{
+                    "feature": {"name": "FOO", "localtype": "DZ"},
+                    "seqno": 1,
+                    "volumes": []
+                }]
+            }]
+        }
+    ]
 }
 
 def create_tmp_text_file(data):
-  f = tempfile.TemporaryFile(mode="w+", encoding="utf-8")
-  f.write(data)
-  f.seek(0)
+    f = tempfile.TemporaryFile(mode="w+", encoding="utf-8")
+    f.write(data)
+    f.seek(0)
 
-  return f
+    return f
 
 def create_tmp_binary_file(data):
-  f = tempfile.TemporaryFile(mode="w+b")
-  f.write(data)
-  f.seek(0)
+    f = tempfile.TemporaryFile(mode="w+b")
+    f.write(data)
+    f.seek(0)
 
-  return f
+    return f
 
 def test_text_read():
-  input = {"test": "¡Hola!"}
-  with create_tmp_text_file(json.dumps(input)) as f:
-    output = yaixm.load(f)
-
-  assert input == output
-
-def test_binary_read():
-  input = {"test": "¡Hola!"}
-  with create_tmp_binary_file(bytes(json.dumps(input), encoding="utf-8")) as f:
-    output = yaixm.load(f)
+    input = {"test": "¡Hola!"}
+    with create_tmp_text_file(json.dumps(input)) as f:
+        output = yaixm.load(f)
 
     assert input == output
 
-def test_sting_read():
-  input = {"test": "¡Hola!"}
-  output = yaixm.load(json.dumps(input))
+def test_binary_read():
+    input = {"test": "¡Hola!"}
+    with create_tmp_binary_file(bytes(json.dumps(input), encoding="utf-8")) as f:
+        output = yaixm.load(f)
 
-  assert input == output
+        assert input == output
+
+def test_sting_read():
+    input = {"test": "¡Hola!"}
+    output = yaixm.load(json.dumps(input))
+
+    assert input == output
 
 def test_json_text_read():
-  input = {'test': "¡Hola!"}
-  with create_tmp_text_file(json.dumps(input)) as f:
-    output = yaixm.load(f, json=True)
+    input = {'test': "¡Hola!"}
+    with create_tmp_text_file(json.dumps(input)) as f:
+        output = yaixm.load(f, json=True)
 
-  assert input == output
+    assert input == output
 
 def test_json_sting_read():
-  input = {"test": "¡Hola!"}
-  output = yaixm.load(json.dumps(input), json=True)
+    input = {"test": "¡Hola!"}
+    output = yaixm.load(json.dumps(input), json=True)
 
-  assert input == output
+    assert input == output
 
 def test_validation_good():
-  input = dict(TEST_AIRSPACE)
-  e = yaixm.validate(input)
-  assert e is None
+    input = dict(TEST_AIRSPACE)
+    e = yaixm.validate(input)
+    assert e is None
 
 def test_validation_bad():
-  input = dict(TEST_AIRSPACE)
-  input['airspace'][0]['type'] = "NOT REALLY A TYPE"
-  e = yaixm.validate(input)
-  assert e
+    input = dict(TEST_AIRSPACE)
+    input['airspace'][0]['type'] = "NOT REALLY A TYPE"
+    e = yaixm.validate(input)
+    assert e
 
 def test_openair():
-  input = dict(TEST_AIRSPACE)
-  oa = yaixm.openair(input['airspace'])
-  assert len(oa) == 6
+    input = dict(TEST_AIRSPACE)
+    oa = yaixm.openair(input['airspace'])
+    assert len(oa) == 6
 
 def test_openair_name():
-  def name_func(feature, volume):
-    return "FOONAME"
+    def name_func(feature, volume):
+        return "FOONAME"
 
-  input = dict(TEST_AIRSPACE)
-  oa = yaixm.openair(input['airspace'], nfunc=name_func)
+    input = dict(TEST_AIRSPACE)
+    oa = yaixm.openair(input['airspace'], nfunc=name_func)
 
-  assert oa[1] == "AN FOONAME"
+    assert oa[1] == "AN FOONAME"
 
 def test_openair_class():
-  def class_func(feature, volume):
-    return "A"
+    def class_func(feature, volume):
+        return "A"
 
-  input = dict(TEST_AIRSPACE)
-  oa = yaixm.openair(input['airspace'], cfunc=class_func)
+    input = dict(TEST_AIRSPACE)
+    oa = yaixm.openair(input['airspace'], cfunc=class_func)
 
-  assert oa[0] == "AC A"
+    assert oa[0] == "AC A"
