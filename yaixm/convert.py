c66c83d640af0ebedf90f69d871807f7b1ab2345
Alan Sparrow <github@freeflight.org.uk>
2017-09-16 18:49:54 +0100

    Add type and class function factories

--- yaixm/convert.py
+++ yaixm/convert.py
@@ -103,70 +103,94 @@ def default_name(volume, feature):
         else:
             return feature['name']
 
-# Default Openair type
-def default_openair_type(volume, feature):
-    as_type = feature['type']
-    local_type = feature.get('localtype')
-    rules = feature.get('rules', []) + volume.get('rules', [])
+# Openair type function
+def make_openair_type(atz="CTR", ils="G"):
+    def openair_type(volume, feature):
+        as_type = feature['type']
+        local_type = feature.get('localtype')
+        rules = feature.get('rules', []) + volume.get('rules', [])
 
-    if as_type == "D" or local_type == "DZ":
-        out_type = "Q"
-    elif as_type == "R":
-        out_type = "R"
-    elif as_type == "P":
-        out_type = "P"
-    elif as_type == "ATZ" or local_type == "ILS":
-        out_type = "CTR"
-    elif local_type == "MATZ":
-        out_type = "MATZ"
-    elif local_type == "TMZ" or "TMZ" in rules:
-        out_type = "TMZ"
-    elif local_type == "RMZ" or "RMZ" in rules:
-        out_type = "RMZ"
-    elif local_type in ["GLIDER", "NOATZ", "UL"]:
-        out_type = "G"
-    else:
-        out_type = volume.get('class') or feature.get('class') or "OTHER"
+        if as_type == "D" or local_type == "DZ":
+            out_type = "Q"
+        elif as_type == "R":
+            out_type = "R"
+        elif as_type == "P":
+            out_type = "P"
+        elif as_type == "ATZ":
+            out_type = atz
+        elif local_type == "ILS":
+            out_type = ils
+        elif local_type == "MATZ":
+            out_type = "MATZ"
+        elif local_type == "TMZ" or "TMZ" in rules:
+            out_type = "TMZ"
+        elif local_type == "RMZ" or "RMZ" in rules:
+            out_type = "RMZ"
+        elif local_type in ["GLIDER", "NOATZ", "UL"]:
+            out_type = "G"
+        else:
+            out_type = volume.get('class') or feature.get('class') or "OTHER"
 
-    return out_type
+        return out_type
 
-# Default TNP class
-def default_tnp_class(volume, feature):
-    local_type = feature.get('localtype')
+    return openair_type
 
-    if volume.get('class'):
-        return volume['class']
-    elif feature.get('class'):
-        return feature['class']
-    else:
-        return None
+default_openair_type = make_openair_type()
 
-# Default TNP type
-def default_tnp_type(volume, feature):
-    as_type = feature['type']
-    local_type = feature.get('localtype')
-    rules = feature.get('rules', []) + volume.get('rules', [])
+# TNP class function
+def make_tnp_class(atz=None, ils=None):
+    def tnp_class(volume, feature):
+        as_type = feature['type']
+        local_type = feature.get('localtype')
 
-    if as_type == "D" or local_type == "DZ":
-        out_type = "DANGER"
-    elif as_type == "P":
-        out_type = "PROHIBITED"
-    elif as_type == "R":
-        out_type = "RESTRICTED"
-    elif as_type in ["ATZ", "CTA", "CTR", "TMA"] or local_type == "ILS":
-        out_type = "CTA/CTR"
-    elif local_type == "MATZ":
-        out_type = "MATZ"
-    elif local_type == "TMZ" or "TMZ" in rules:
-        out_type = "TMZ"
-    elif local_type == "RMZ" or "RMZ" in rules:
-        out_type = "RMZ"
-    elif as_type == "AWY":
-        out_type = "AIRWAYS"
-    else:
-        out_type = "OTHER"
+        if volume.get('class'):
+            return volume['class']
+        elif feature.get('class'):
+            return feature['class']
+        elif as_type == "ATZ":
+            return atz
+        elif local_type == "ILS":
+            return ils
+        else:
+            return None
 
-    return out_type
+    return tnp_class
+
+default_tnp_class = make_tnp_class()
+
+# TNP type function
+def make_tnp_type(ils="OTHER"):
+    def tnp_type(volume, feature):
+        as_type = feature['type']
+        local_type = feature.get('localtype')
+        rules = feature.get('rules', []) + volume.get('rules', [])
+
+        if as_type == "D" or local_type == "DZ":
+            out_type = "DANGER"
+        elif as_type == "P":
+            out_type = "PROHIBITED"
+        elif as_type == "R":
+            out_type = "RESTRICTED"
+        elif as_type in ["ATZ", "CTA", "CTR", "TMA"]:
+            out_type = "CTA/CTR"
+        elif local_type == "MATZ":
+            out_type = "MATZ"
+        elif local_type == "TMZ" or "TMZ" in rules:
+            out_type = "TMZ"
+        elif local_type == "RMZ" or "RMZ" in rules:
+            out_type = "RMZ"
+        elif as_type == "AWY":
+            out_type = "AIRWAYS"
+        elif local_type == "ILS":
+            out_type = ils
+        else:
+            out_type = "OTHER"
+
+        return out_type
+
+    return tnp_type
+
+default_tnp_type = make_tnp_type()
 
 # Base class for TNP and OpenAir converters
 class Converter():
