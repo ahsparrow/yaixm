5132e7cad1d57375a82df2c6e58c671da9f67f9e
Alan Sparrow <git@freeflight.org.uk>
2018-03-01 10:17:09 +0000

    Version 1.3.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="yaixm",
-    version="1.3.6",
+    version="1.3.7",
     description="YAML/JSON simplified AIXM",
     url="https://github.com/ahsparrow/yaixm",
     author="Alan Sparrow",
